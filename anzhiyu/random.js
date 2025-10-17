d4ae15e2788a5605cd0b5a11415b3d061477dc0d
Chank_Xia <15526058208@qq.com>
2025-10-18 00:47:58 +0800

    Site updated: 2025-10-18 00:47:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/17/hello-world/"];function toRandomPost(){
+var posts=["2025/10/18/终于搞完了/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
